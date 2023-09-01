4d242a2df058690b95764d7c78fb0aa3ccd3913d
fjjosjsso <sakaraysairam1996@gmail.com>
2023-09-01 16:55:58 +0530

    REPLACED x and y

---
+++ Evening_conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=x
+PERSON2=y
+echo "$PERSON1 : Hello $PERSON2, Good Morning "
+echo "$PERSON2 : Hello $PERSON1, Good Morning, How are you handsome "
+echo "$PERSON1 : Iam good what about you darling, does really iam looking handsome"
+echo "$PERSON2 : Yes Handsome your looking like puka na munda na koddaka"
+echo "$PERSON2 : Dengutha koddaka "
