f54fa612b555f2b1b40799d7be0a1a42baa36b6c
fjjosjsso <sakaraysairam1996@gmail.com>
2023-09-01 16:32:16 +0530

    Changed name RAM AND SURESH TO SAI AND NANI

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=SAI
+PERSON2=NANI
+echo "$PERSON1 : Hello $PERSON2. Good morning"
+echo "$PERSON2 : Hi $PERSON1 . Good morning, How are you"
+echo "$PERSON1 : I AM Good thanks for asking"
+echo "$PERSON2 : Ha thanks tesukoni guddha lo petuko puka"
